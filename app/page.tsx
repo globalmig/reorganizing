7aa0e4f439d1893f8ff45732a1225b4e3ca88139
globalmig <wdpb@mig.kr>
2026-03-30 11:40:15 +0900

    edit: 개편중, 카달로그 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,142 @@
+import SectionTitle from "@/components/common/SectionTitle";
+import MainSlide from "@/components/slide/MainSlide";
+import { MainTitle, SubTitle } from "@/components/style/style";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="development">
-    <div>
-      <Image className="pc" src="/images/development_img.png" alt="현재 홈페이지 개편을 위한 작업중입니다." width={2560} height={1440}/>
-      <Image className="mo" src="/images/development_mo.png" alt="현재 홈페이지 개편을 위한 작업중입니다." width={500} height={526}/>
-    </div>
-      {/* <article>
+    <>
+      <div className="development">
+        <div>
+          <Image className="pc" src="/images/development_img.png" alt="현재 홈페이지 개편을 위한 작업중입니다." width={2560} height={1440} />
+          <Image className="mo" src="/images/development_mo.png" alt="현재 홈페이지 개편을 위한 작업중입니다." width={500} height={526} />
+        </div>
+      </div>
+
+      <div className="development2">
+        {Array(6).fill(0).map((_, index) => (
+          <div key={index}>
+            <Image
+              src={`/images/001/${index + 1}.png`}
+              alt={`에코원 카탈로그 ${index + 1}`}
+              width={1293}
+              height={916}
+            />
+          </div>
+        ))}
+      </div>
+
+
+      {/* <main className="home-banner">
+        <div>
+          <div className="slider-wrapper">
+            <MainSlide />
+          </div>
+          <div>
+            <div>
+              <Image src="/images/하루의활기찬시작.png" alt="하루의 활기찬 시작" width={100} height={100} />
+              <h2>단순한 활력을 넘어 몸속 깊은 곳의 균형까지</h2>
+              <p>우리들의 하루를 더 단단하고 건강하게 지켜줍니다.</p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <article className="home1">
         <div>
 
+          <div>
+            <div>
+              <SubTitle>자연이 준 최대의 선물!</SubTitle>
+              <MainTitle>귀를 기울이면 들려오는 자연의 속삭임<br />좋은 에너지는 건강이 됩니다.</MainTitle>
+            </div>
+            <p>‘원적외선의 과학기술이 숙면 과학의 기술’ 입니다.<br />
+              칠보복합세라믹으로 가족의 건강을 되찾아드리겠습니다.<br />
+              가족 건강을 위해 ECHOONE과 함께 하십시오.</p>
+            <div>
+              <Link href="/product" className="display-flex">
+                <p>READ MORE</p>
+                <div>
+                  <Image src="/icons/arrow_red.png" alt="자세히 보기 아이콘" width={100} height={100} />
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <div className="display-flex-flow">
+            <div className="pc"></div>
+            <section>
+              <Link href="/product/heating">
+                <div>
+                  <Image src="/icons/전신온열케어.png" alt="전신 온열 케어 제품 보기" width={100} height={100} />
+                </div>
+                <h3>전신 온열 케어</h3>
+                <p>전신을 따뜻하게 감싸고 파동 에너지를 전달하여 내일의 에너지를 충전합니다.</p>
+              </Link>
+            </section>
+            <div className="pc"></div>
+            <section>
+              <Link href="/product/intensive">
+                <div>
+                  <Image src="/icons/부분집중케어.png" alt="부분 집중 케어 제품 보기" width={100} height={100} />
+                </div>
+                <h3>부분 집중 케어</h3>
+                <p>신체 특정 부위의 체온을 올리고 통증 완화를 도와 따뜻한 휴식을 드립니다.</p>
+              </Link>
+            </section>
+            <div className="pc"></div>
+            <div className="pc"></div>
+          </div>
+
+          <div>
+            <Image src="/images/소파형라온드링.png" alt="쇼파형 라온드림" width={500} height={500} />
+          </div>
+
+        </div >
+      </article >
+
+      <article className="home2">
+        <div>
+
+          <div>
+            <Image src="/images/.png" alt="쇼파형 라온드림" width={500} height={500} />
+          </div>
+
+          <div>
+            <div>
+              <SubTitle>에코원이 만들어낸 건강과학</SubTitle>
+              <MainTitle>어제보다 건강한 오늘,<br />칠보세라믹으로부터</MainTitle>
+            </div>
+            <p>칠보세라믹은 백두대간의 태백산 줄기에 위치한 일월산에서 1억 5천만년 전 형성된 퇴적암으로 열에너지파의 일종인 적외선 영역(0.76~1000um)에는 원적외선 파장대가 있어 모든 생물의 성장발육과 생명활동을 촉진시키는 신비의 빛 생육광선이 있습니다. <br /><br />열에너지의 효율이 가장 강력한 파장대(6~14um)의 원적외선 중 방출량이 가장 우수한 소재를 선별, 명물질을 블랜딩시켜, 원료정제, 혼합 등 철저한 성분관리를 하였으며, 또한 고온으로 소결하여 원적외선이 고방사되는 복사세라믹입니다.</p>
+            <div>
+              <Link href="/company" className="display-flex">
+                <p>READ MORE</p>
+                <div>
+                  <Image src="/icons/arrow_red.png" alt="자세히 보기 아이콘" width={100} height={100} />
+                </div>
+              </Link>
+            </div>
+          </div>
+
+        </div>
+      </article>
+
+      <article className="home3">
+        <div>
+          <SectionTitle main="칠보세라믹의 헬스테라피" sub="통증은 멀게, 건강은 가깝게" />
+          <div className="display-flex-flow">
+            <section>
+              <div>
+                <div>
+                  <Image src="/images/proudct/.png" alt="" width={100} height={100}/>
+                  <p>라온블랙다이아</p>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
       </article> */}
-    </div>
+    </>
   );
 }
